93312b46d7605bc8657e370690c6996785cde703
Fuser <109461244+Fuyuza@users.noreply.github.com>
2023-01-15 19:58:13 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="aiowaifu",
+  version="0.1",
+  long_description=open("README.md").read(),
+  long_description_content_type='text/markdown',
+  packages=["aiohttp"],
+  keywords="waifu anime neko",
+)
